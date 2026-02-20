340f3efcd5f1c3eb163db00c26d828005f042fba
Mateo Juretić <145344294+Mutija@users.noreply.github.com>
2026-02-20 16:16:06 +0100

    Delete WeCollect directory

--- WeCollect/app/src/main/java/com/wedesign/wecollectfirebase/CaptureAct.java
+++
@@ -1,6 +0,0 @@
-package com.wedesign.wecollectfirebase;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class CaptureAct extends CaptureActivity {
-}
--- WeCollect/app/src/main/java/com/wedesign/wecollectfirebase/RecyclerViewInterface.java
+++
@@ -1,5 +0,0 @@
-package com.wedesign.wecollectfirebase;
-
-public interface RecyclerViewInterface {
-    void onItemClick(int position);
-}
